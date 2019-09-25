766c6365be5799d9ed24ab1c9d4a02230522b76f
mohamed mohamed <mohamed.mohamed@vodafone.com>
2019-09-25 20:12:17 +0200

    adding new linting checker file

--- outputscript.sh
+++ outputscript.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 echo "Adding Windows output"
-zip windowsoutput_$BUILD_NUMBER.zip HelloWorldSolution/windows windowsbuildlogfile.txt
+zip windowsoutput_$BUILD_NUMBER.zip HelloWorldSolution/windows windowsbuildlogfile.txt lint_checker_output.txt
 echo "Adding Ubunu output"
-zip ubuntuoutput_$BUILD_NUMBER.zip  HelloWorldSolution/ubuntu ubuntubuildlogfile.txt 
+zip ubuntuoutput_$BUILD_NUMBER.zip  HelloWorldSolution/ubuntu ubuntubuildlogfile.txt lint_checker_output.txt 
