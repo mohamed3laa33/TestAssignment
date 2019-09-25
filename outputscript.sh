20f2ce988bca4a46d99e39e3cea577c8fb1ef96b
mohamed mohamed <mohamed.mohamed@vodafone.com>
2019-09-25 22:02:01 +0200

    adding comments to Jenkinns file

--- outputscript.sh
+++ outputscript.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 echo "Adding Windows output"
-zip windowsoutput_$BUILD_NUMBER.zip HelloWorldSolution/windows_build windowsbuildlogfile.txt lint_checker_output.txt
+zip windowsoutput_$BUILD_NUMBER.zip HelloWorldSolution/windows_build windowsbuildlogfile.txt lint_checker_output.txt Build_output.txt
 echo "Adding Ubunu output"
-zip ubuntuoutput_$BUILD_NUMBER.zip  HelloWorldSolution/ubuntu_build ubuntubuildlogfile.txt lint_checker_output.txt 
+zip ubuntuoutput_$BUILD_NUMBER.zip  HelloWorldSolution/ubuntu_build ubuntubuildlogfile.txt lint_checker_output.txt  Build_output.txt
