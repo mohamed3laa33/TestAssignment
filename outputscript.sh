7b5d74967de1b75c87c82e84236e03e549de18d8
mohamed mohamed <mohamed.mohamed@vodafone.com>
2019-09-25 19:36:22 +0200

    adding echo to output.sh file

--- outputscript.sh
+++ outputscript.sh
@@ -1,3 +1,5 @@
-#! /bin/sh 
+#! /bin/sh
+echo "Adding Windows output"
 zip windowsoutput_$BUILD_NUMBER.zip HelloWorldSolution/windows windowsbuildlogfile.txt
+echo "Adding Ubunu output"
 zip ubuntuoutput_$BUILD_NUMBER.zip  HelloWorldSolution/ubuntu ubuntubuildlogfile.txt 
